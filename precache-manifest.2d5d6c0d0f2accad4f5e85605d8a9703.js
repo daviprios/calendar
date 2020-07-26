2749de93d573571da0f370096285d0a66095c926
Mordecai210 <mordecai210@users.noreply.github.com>
2020-07-26 17:41:03 -0300

    Updates

---
+++ precache-manifest.2d5d6c0d0f2accad4f5e85605d8a9703.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5aa26e95bdab5110f2e6db42615e6fe",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "b32c3dde392f8268d40a",
+    "url": "/calendar/static/css/main.9531f695.chunk.css"
+  },
+  {
+    "revision": "9de9d90608d865f02211",
+    "url": "/calendar/static/js/2.4f137075.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calendar/static/js/2.4f137075.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b32c3dde392f8268d40a",
+    "url": "/calendar/static/js/main.9cfe5c2a.chunk.js"
+  },
+  {
+    "revision": "0d5c1feacd1219e7629f",
+    "url": "/calendar/static/js/runtime-main.ad1f5134.js"
+  }
+]);
