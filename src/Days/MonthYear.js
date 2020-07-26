ef79080e063aac26567914312f851d46188e414c
Mordecai210 <mordecai210@users.noreply.github.com>
2020-07-26 17:33:15 -0300

    Initial commit

---
+++ src/Days/MonthYear.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './MonthYear.css';
+
+export default class MonthYearSearch extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      month: this.props.currentMonth,
+      year: this.props.currentYear,
+    };
+
+    this.eventHandler = this.eventHandler.bind(this);
+    this.Verify = this.Verify.bind(this);
+  }
+
+  Verify(event){
+    if(this.state.month > 12 || this.state.month < 1 || this.state.year === 0)
+      return;
+    this.props.changeDate(this.state.month, this.state.year);
+    event.preventDefault();
+  }
+
+  eventHandler(event){
+    if(isNaN(Number(event.target.value)) || String(event.target.value).length > 4)
+      return;
+    this.setState({ [event.target.id]: Number(event.target.value) });
+  }
+
+  render() {
+    return (
+      <div className="form-container">
+        <form className="form-grid" onSubmit={this.Verify}>
+          <span className="label" >Month:</span>
+          <input className="input" id="month" type="text" onChange={this.eventHandler} value={this.state.month} ></input>
+          <span className="label" >Year:</span>
+          <input className="input" id="year" type="text" onChange={this.eventHandler} value={this.state.year} ></input>
+          <button className="button" type="submit">GO!</button>
+        </form>
+      </div>
+    )
+  }
+}
